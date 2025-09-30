25cd525b2e7d745e4136470e4b28d30e04f5a2ac
Dmitry <dmytro.oborskiy@angleto.com>
2025-09-30 14:53:44 +0300

    feat: setup svg sprite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import vituum from "vituum";
 import nunjucks from "@vituum/vite-plugin-nunjucks";
+import VitePluginSvgSpritemap from "@spiriit/vite-plugin-svg-spritemap";
 
 export default defineConfig({
   plugins: [
+    VitePluginSvgSpritemap("./src/assets/icons/*.svg", {
+      prefix: "icon-",
+    }),
     vituum(),
     nunjucks({
       root: "./src",
