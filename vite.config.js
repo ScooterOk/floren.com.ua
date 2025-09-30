e68708dcf7ce94aafa14dadedc765c26e7847cf7
Dmitry <dmytro.oborskiy@angleto.com>
2025-09-30 14:08:41 +0300

    init project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import vituum from "vituum";
+import nunjucks from "@vituum/vite-plugin-nunjucks";
+
+export default defineConfig({
+  plugins: [
+    vituum(),
+    nunjucks({
+      root: "./src",
+    }),
+  ],
+});
