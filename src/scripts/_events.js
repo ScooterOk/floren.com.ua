ef9cc0603f7ebe647945481e4c19575fefe37c9d
Dmitry <dmytro.oborskiy@angleto.com>
2025-10-05 19:12:04 +0300

    feat: header phones

---
+++ src/scripts/_events.js
@@ -0,0 +1,26 @@
+const clickHandlers = {
+  togglePhones: (event) => {
+    const element = event.currentTarget;
+    const tooltip = element.closest('#phones-tooltip');
+    const wrapper = tooltip.querySelector('.header__main--phones');
+
+    tooltip.open = !tooltip.open;
+    wrapper.classList.toggle('active');
+  },
+};
+
+export const initEvents = () => {
+  const targets = document.querySelectorAll('[data-event]');
+  targets.forEach((el) => {
+    const event = el.dataset.event;
+    const callback = clickHandlers[el.dataset.callback];
+    if (!callback) {
+      console.warn(
+        `Callback function "${el.dataset.callback}" not found for element:`,
+        el
+      );
+      return;
+    }
+    el.addEventListener(event, callback);
+  });
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,10 @@ import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
 import '@shoelace-style/shoelace/dist/components/menu/menu.js';
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
 import '@shoelace-style/shoelace/dist/components/divider/divider.js';
+
+import { initEvents } from './_events';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('DOM fully loaded and parsed');
+  initEvents();
+});
